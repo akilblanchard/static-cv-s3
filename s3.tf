42470e7d795769a0e8d92b0c1a3298237f1e3531
AkilBlanchard <akilblanchard@github.com>
2023-02-02 18:15:58 -0600

    created s3 bucket for cv and attached policy

--- s3.tf
+++ s3.tf
@@ -1,9 +1,41 @@
+#cv_bucket
 resource "aws_s3_bucket" "cv_bucket" {
   bucket = "akil-blanchard.com"
   acl    = "public-read"
+
 
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
+  tags = {
+    Enviroment = "development"
+    name       = "cv-bucket"
+  }
 }
+
+#s3 policy
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.cv_bucket.id
+  policy = <<POLICY
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+	  "Principal": "*",
+      "Action": [ "s3:*" ],
+      "Resource": [
+        "${aws_s3_bucket.cv_bucket.arn}",
+        "${aws_s3_bucket.cv_bucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+
+
