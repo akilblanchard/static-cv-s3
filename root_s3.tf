5a65ab36b5bb644bac5fcd9b92ad6a5c8b912f63
AkilBlanchard <akilblanchard@github.com>
2023-02-12 18:29:56 -0600

    configuring second bucket for redirecting to the www version of website

---
+++ root_s3.tf
@@ -0,0 +1,11 @@
+#S3 bucket for directing www. traffic
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+
+  website {
+    redirect_all_requests_to = "https://www.{var.domain_name}"
+  }
+}
+
