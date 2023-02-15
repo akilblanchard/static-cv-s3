a1cd8e92eb3cb3f10b29b16eb82ce8c67e8b5800
AkilBlanchard <akilblanchard@github.com>
2023-02-14 23:20:09 -0600

    attached policy to bucket

--- root_s3.tf
+++ root_s3.tf
@@ -1,6 +1,7 @@
 #S3 bucket for directing www. traffic
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
+  policy = templatefile("s3-policy.json", { bucket = var.bucket_name })
   acl    = "public-read"
 
 
