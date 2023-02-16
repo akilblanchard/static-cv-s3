becebf0aa307d2469c79b2246cd953507102e6c5
AkilBlanchard <akilblanchard@github.com>
2023-02-16 00:57:08 -0600

    validated certificates

--- acm.tf
+++ acm.tf
@@ -1,1 +1,5 @@
-#SSL Certificate
+resource "aws_acm_certificate" "ssl_certificate" {
+  domain_name       = "akilblanchard.com"
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method = "DNS"
+}
