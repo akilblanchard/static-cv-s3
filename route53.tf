a40c3ebe45d229171d9099303842510424e842e4
AkilBlanchard <akilblanchard@github.com>
2023-02-19 17:08:03 -0600

    updated alias name

--- route53.tf
+++ route53.tf
@@ -10,8 +10,8 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website_bucket.domain_name
+    zone_id                = aws_cloudfront_distribution.website_bucket.hosted_zone_id
     evaluate_target_health = false
   }
 }
