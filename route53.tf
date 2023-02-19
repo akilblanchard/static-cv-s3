1e12b49086fa5e20543467e052d60c990c0d1e8d
AkilBlanchard <akilblanchard@github.com>
2023-02-19 10:39:08 -0600

    cleaned up file

--- route53.tf
+++ route53.tf
@@ -1,10 +1,10 @@
-#records for www and non-www 
+#records for bucket
 resource "aws_route53_zone" "main" {
   name = var.domain_name
   tags = var.common_tags
 }
 
-resource "aws_route53_record" "www-a" {
+resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
