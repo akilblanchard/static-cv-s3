97c0e33665c87fc07fe10efdb0cf2eee42d64829
AkilBlanchard <akilblanchard@github.com>
2023-02-19 10:37:46 -0600

    configured bucket

---
+++ route53.tf
@@ -0,0 +1,17 @@
+#records for www and non-www 
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+  tags = var.common_tags
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
